b8cabf39b03a486e71ba9731fe9ee31f2814897c
Dillz42 <dillz42@gmail.com>
2017-01-28 20:58:02 -0600

    Haven't commited for a while...

---
+++ CloverLibrary/Global.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+
+namespace CloverLibrary
+{
+    public class Global
+    {
+        public const string BASE_URL = "http://a.4cdn.org/";
+        public const string BASE_IMAGE_URL = "http://i.4cdn.org/";
+        public const string DEFAULT_IMAGE = "http://s.4cdn.org/image/fp/logo-transparent.png";
+
+        private static SortedDictionary<int, ChanPost> postDictionary = new SortedDictionary<int, ChanPost>();
+
+        public async static Task<List<Tuple<string, string, string>>> getBoardList(CancellationToken cancellationToken = new CancellationToken())
+        {
+            List<Tuple<string, string, string>> retVal = new List<Tuple<string, string, string>>();
+
+            JArray boardList =
+                (JArray)(
+                    (JObject)(
+                        await WebTools.httpRequestParse(BASE_URL + "boards.json", JObject.Parse, cancellationToken)
+                    )
+                )["boards"];
+
+            foreach (JObject board in boardList)
+            {
+                retVal.Add(Tuple.Create(
+                    board["board"].ToString(),
+                    board["title"].ToString(),
+                    System.Net.WebUtility.HtmlDecode(board["meta_description"].ToString())));
+            }
+
+            return retVal;
+        }
+
+        public async static Task loadBoard(string board = "b", CancellationToken cancellationToken = new CancellationToken())
+        {
+            string address = BASE_URL + board + "/catalog.json";
+            JArray jsonArray = (JArray)await WebTools.httpRequestParse(address, JArray.Parse);
+
+            foreach (JObject boardPage in jsonArray)
+            {
+                foreach (JObject jsonThread in boardPage["threads"])
+                {
+                    if (postDictionary.ContainsKey((int)jsonThread["no"]) == false)
+                    {
+                        postDictionary.Add((int)jsonThread["no"], new ChanPost(jsonThread, board));
+                    }
+                }
+            }
+        }
+
+        public static List<ChanPost> getBoard(string board, CancellationToken cancellationToken = new CancellationToken())
+        {
+            List<ChanPost> retVal = new List<ChanPost>();
+
+            foreach (var post in postDictionary)
+            {
+                if(cancellationToken.IsCancellationRequested)
+                {
+                    break;
+                }
+                if (post.Value.board == board)
+                {
+                    retVal.Add(post.Value);
+                }
+            }
+            retVal.Sort();
+            return retVal;
+        }
+
+        public async static Task loadThread(int threadNumber, string board = "b", CancellationToken cancellationToken = new CancellationToken())
+        {
+            string address = BASE_URL + board + "/thread/" + threadNumber + ".json";
+            JObject jsonObject = (JObject)await WebTools.httpRequestParse(address, JObject.Parse);
+
+            foreach (JObject jsonThread in (JArray)jsonObject["posts"])
+            {
+                if (postDictionary.ContainsKey((int)jsonThread["no"]) == false)
+                {
+                    postDictionary.Add((int)jsonThread["no"], new ChanPost(jsonThread, board));
+                }
+            }
+        }
+
+        public static List<ChanPost> getThread(int threadNumber, string board = "b", CancellationToken cancellationToken = new CancellationToken())
+        {
+            List<ChanPost> retVal = new List<ChanPost>();
+
+            foreach (var post in postDictionary)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    break;
+                }
+                if (post.Value.resto == threadNumber || post.Value.no == threadNumber)
+                {
+                    retVal.Add(post.Value);
+                }
+            }
+            retVal.Sort();
+            return retVal;
+        }
+    }
+}
--- CovertClover/MainWindow.xaml.cs
+++ CovertClover/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
 
 namespace CovertClover
 {
@@ -20,9 +21,194 @@ namespace CovertClover
     /// </summary>
     public partial class MainWindow : Window
     {
+        System.Threading.CancellationTokenSource tokenSource = new System.Threading.CancellationTokenSource();
         public MainWindow()
         {
             InitializeComponent();
+
+            //CloverLibrary.Global.testDownload();
+
+            Task<List<Tuple<string, string, string>>> boardListTask = CloverLibrary.Global.getBoardList();
+            boardListTask.ContinueWith(t =>
+            {
+                switch (t.Status)
+                {
+                    case TaskStatus.RanToCompletion:
+                        foreach (var board in t.Result)
+                        {
+                            Button boardButton = new Button();
+                            boardButton.MinWidth = 100;
+                            boardButton.Margin = new Thickness(1);
+                            boardButton.Content = board.Item1;
+                            boardButton.Click += BoardButton_Click;
+                            boardButton.DataContext = board;
+                            BoardList.Children.Add(boardButton);
+
+                            ToolTip toolTip = new ToolTip();
+                            toolTip.Content = board.Item2 + "\n" + board.Item3;
+                            toolTip.Placement = System.Windows.Controls.Primitives.PlacementMode.Right;
+                            ToolTipService.SetToolTip(boardButton, toolTip);
+                        }
+                        break;
+                    case TaskStatus.Canceled:
+                        MessageBox.Show("boardListTask was Canceled!", "CANCELED");
+                        System.Diagnostics.Debugger.Break();
+                        break;
+                    case TaskStatus.Faulted:
+                        MessageBox.Show("boardListTask was Faulted!", "FAULTED");
+                        System.Diagnostics.Debugger.Break();
+                        break;
+                }
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+        
+        private async void BoardButton_Click(object sender, RoutedEventArgs e)
+        {
+            tokenSource.Cancel();
+            tokenSource = new System.Threading.CancellationTokenSource();
+
+            ThreadList.Children.Clear();
+            ((ScrollViewer)ThreadList.Parent).ScrollToTop();
+
+            string board = ((Tuple<string, string, string>)((Button)sender).DataContext).Item1;
+            Title = board + " - " + ((Tuple<string, string, string>)((Button)sender).DataContext).Item2;
+
+            await CloverLibrary.Global.loadBoard(board, tokenSource.Token);
+
+            List<CloverLibrary.ChanPost> postList = CloverLibrary.Global.getBoard(board, tokenSource.Token);
+            foreach (CloverLibrary.ChanPost post in postList)
+            {
+                try
+                {
+                    ThreadList.Children.Add(await convertPostToStackPanel(post));
+                }
+                catch (TaskCanceledException)
+                {
+                    break;
+                }
+                catch (Exception)
+                {
+                    System.Diagnostics.Debugger.Break();
+                    throw;
+                }
+            }
+        }
+
+        public async void ThreadButton_Click(object sender, RoutedEventArgs e)
+        {
+            tokenSource.Cancel();
+            tokenSource = new System.Threading.CancellationTokenSource();
+
+            ThreadList.Children.Clear();
+            ((ScrollViewer)ThreadList.Parent).ScrollToTop();
+
+            CloverLibrary.ChanPost senderPost = ((CloverLibrary.ChanPost)((Button)sender).DataContext);
+            await CloverLibrary.Global.loadThread(senderPost.no, senderPost.board, tokenSource.Token);
+
+            List<CloverLibrary.ChanPost> postList = CloverLibrary.Global.getThread(senderPost.no, senderPost.board, tokenSource.Token);
+            foreach (CloverLibrary.ChanPost post in postList)
+            {
+                try
+                {
+                    ThreadList.Children.Add(await convertPostToStackPanel(post));
+                }
+                catch (TaskCanceledException)
+                {
+                    break;
+                }
+                catch (Exception)
+                {
+                    System.Diagnostics.Debugger.Break();
+                    throw;
+                }
+            }
+        }
+
+        private async Task<FrameworkElement> convertPostToStackPanel(CloverLibrary.ChanPost post)
+        {
+            StackPanel retVal = new StackPanel();
+
+            await post.loadThumb(tokenSource.Token);
+
+            BitmapImage source = new BitmapImage();
+            source.BeginInit();
+            source.StreamSource = new System.IO.MemoryStream(post.thumbData);
+            source.CacheOption = BitmapCacheOption.OnLoad;
+            source.EndInit();
+            source.Freeze();
+
+            Image img = new Image();
+            img.Source = source;
+            img.MaxWidth = post.tn_w;
+            img.HorizontalAlignment = HorizontalAlignment.Left;
+
+            ToolTip imageToolTip = new ToolTip();
+            StackPanel toolTipStackPanel = new StackPanel();
+            TextBlock toolTipTextBlock = new TextBlock();
+            Image toolTipImage = new Image();
+
+            toolTipStackPanel.Orientation = Orientation.Vertical;
+
+            toolTipTextBlock.Text = post.tim + "-" + post.filename + post.ext + " - " + post.w + " x " + post.h;
+            toolTipStackPanel.Children.Add(toolTipTextBlock);
+
+            imageToolTip.Loaded += async (ls, le) =>
+            {
+                await post.loadImage();
+                BitmapImage imageToolTipSource = new BitmapImage();
+                imageToolTipSource.BeginInit();
+                imageToolTipSource.StreamSource = new System.IO.MemoryStream(post.imageData);
+                imageToolTipSource.CacheOption = BitmapCacheOption.OnLoad;
+                imageToolTipSource.EndInit();
+                imageToolTipSource.Freeze();
+                toolTipImage.Source = imageToolTipSource;
+                double workingHeight = SystemParameters.WorkArea.Height * .95;
+                toolTipImage.MaxWidth = post.w > SystemParameters.WorkArea.Width ? SystemParameters.WorkArea.Width : post.w;
+                toolTipImage.MaxHeight = post.h > workingHeight ? workingHeight : post.h;
+            };
+            toolTipStackPanel.Children.Add(toolTipImage);
+            imageToolTip.Content = toolTipStackPanel;
+            ToolTipService.SetShowDuration(img, int.MaxValue);
+            ToolTipService.SetInitialShowDelay(img, 0);
+            ToolTipService.SetToolTip(img, imageToolTip);
+
+            TextBlock textBlockSubject = new TextBlock();
+            textBlockSubject.Text = post.board + "/" + post.no + ((post.sub == "") ? ("") : (" - " + post.sub)) +
+                " - R: " + post.replies + " / I: " + post.images;
+            textBlockSubject.TextWrapping = TextWrapping.Wrap;
+
+            TextBlock textBlockComment = new TextBlock();
+            textBlockComment.Text = post.com;
+            textBlockComment.TextWrapping = TextWrapping.Wrap;
+
+            retVal.Children.Add(textBlockSubject);
+            retVal.Children.Add(img);
+            retVal.Children.Add(textBlockComment);
+            
+            if (post.resto == 0)
+            {
+                Button button = new Button();
+
+                button.HorizontalContentAlignment = HorizontalAlignment.Left;
+                button.Content = retVal;
+                button.Click += (cs, ce) =>
+                {
+                    Button threadButton = new Button();
+                    threadButton.Content = post.board + "/" + post.no + "-" + post.sub;
+                    threadButton.Click += ThreadButton_Click;
+                    threadButton.DataContext = post;
+                    ThreadWatchList.Children.Add(threadButton);
+                };
+
+                return button; 
+            }
+            else
+            {
+                Separator seperator = new Separator();
+                retVal.Children.Add(seperator);
+
+                return retVal;
+            }
         }
     }
 }
